f6874390a174ba22a6f2f342f4589e5c2cbebeed
Meysam <Meysam@MGH>
2022-12-21 09:03:34 +0330

    some change in library

--- Mgh.Swagger/Extensions/RegisterSwaggerServiceExtensions.cs
+++ Mgh.Swagger/Extensions/RegisterSwaggerServiceExtensions.cs
@@ -1,83 +1,26 @@
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 
 namespace Mgh.Swagger.Extensions;
 
 public static class RegisterSwaggerServiceExtensions
 {
-    public static IServiceCollection AddSwagger(this IServiceCollection services,
-        Action<ISwaggerConfig> options)
+    public static IServiceCollection AddSwagger(this IServiceCollection services
+        , Action<ISwaggerConfig> options)
     {
-        if (options == null)
-            throw new ArgumentException(nameof(options), "please provide config");
-
         var config = new SwaggerConfig();
         options.Invoke(config);
-
-        if (string.IsNullOrEmpty(config.OpenApiInfo.Description))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiInfo.Description), "Description null");
-
-        if (string.IsNullOrEmpty(config.OpenApiInfo.Version))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiInfo.Version), "Version null");
-
-        if (string.IsNullOrEmpty(config.OpenApiInfo.Title))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiInfo.Title), "Title null");
-
-        if (string.IsNullOrEmpty(config.XmlComments))
-            throw new ArgumentNullException(nameof(SwaggerConfig.XmlComments), "XmlComments null");
-
-        if (string.IsNullOrEmpty(config.OpenApiSecurityScheme.Description))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiSecurityScheme.Description),
-                "Description null");
-
-        if (string.IsNullOrEmpty(config.OpenApiSecurityScheme.Name))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiSecurityScheme.Name), "Name is null");
-
-        if (string.IsNullOrEmpty(config.OpenApiSecurityScheme.Scheme))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiSecurityScheme.Scheme),
-                "SecuritySchema.Schema is null");
-
-
-        if (string.IsNullOrEmpty(config.OpenApiReference.Id))
-            throw new ArgumentNullException(nameof(SwaggerConfig.OpenApiReference.Id), "SecurityReferenceId is null");
-
-        if (string.IsNullOrEmpty(config.AddSecurityDefinitionTitle))
-            throw new ArgumentNullException(nameof(SwaggerConfig.AddSecurityDefinitionTitle),
-                "AddSecurityDefinitionTitle is null");
+        Console.WriteLine(config);
 
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc(config.OpenApiInfo.Title, new OpenApiInfo
-            {
-                Title = config.OpenApiInfo.Title,
-                Version = config.OpenApiInfo.Version,
-                Description = config.OpenApiInfo.Description
-            });
-
-            c.IncludeXmlComments(config.XmlComments);
-            var securitySchema = new OpenApiSecurityScheme
-            {
-                Description =
-                    config.OpenApiSecurityScheme.Description,
-                Name = config.OpenApiSecurityScheme.Name,
-                In = config.OpenApiSecurityScheme.In,
-                Type = config.OpenApiSecurityScheme.Type,
-                Scheme = config.OpenApiSecurityScheme.Scheme,
-                Reference = new OpenApiReference
-                {
-                    Type = config.OpenApiReference.Type,
-                    Id = config.OpenApiReference.Id
-                }
-            };
-
-            c.AddSecurityDefinition(config.AddSecurityDefinitionTitle, securitySchema);
-
-            var securityRequirement = new OpenApiSecurityRequirement
-            {
-                { securitySchema, config.SecurityRequirementSchema }
-            };
-
-            c.AddSecurityRequirement(securityRequirement);
+            if (!string.IsNullOrEmpty(config.XmlComments))
+                c.IncludeXmlComments(config.XmlComments);
+            
+            c.SwaggerDoc(config.OpenApiInfo.Version, config.OpenApiInfo);
+            var securitySchema = config.OpenApiSecurityScheme;
+            securitySchema.Reference = config.OpenApiReference;
+            c.AddSecurityDefinition("Bearer", securitySchema);
+            c.AddSecurityRequirement(config.OpenApiSecurityRequirement);
         });
         return services;
     }
